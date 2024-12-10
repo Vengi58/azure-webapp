f6c9bde68ad138c02d2dbb04ad70fe7073d2e842
Vengi58 <tamas.veingartner@gmail.com>
2024-12-10 23:25:53 +0000

    add test project

---
+++ WeatherApi/WeatherApiTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WeatherApiTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
